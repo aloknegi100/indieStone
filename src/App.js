b115e0b11ae5d51ac5cbda37e8525c2f48e02d1c
Alok Negi <aloknegi@Aloks-MacBook-Air.local>
2023-08-01 21:50:24 +0530

    blog component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Collections from "./pages/Collections";
 import Form from "./pages/Form/Form";
+import Blog from "./pages/Blog/Blog";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/collections" element={<Collections />} />
         <Route path="/my-account" element={<Form />} />
       </Routes>
---
+++ src/constants/blog.js
@@ -0,0 +1,66 @@
+export const blog1 = {
+  introHead: [
+    {
+      text: "Sometimes, overhauling a small space can feel just as daunting as tackling a large one, and bathrooms are no exception. While your budget might stretch a little further, you can end up facing more issues and restrictions, such as tricky plumbing, an awkward layout and frustrating storage limitations. And that’s before you even think about trying to narrow down which tiles will work best in your space.",
+      fontSize: "28px",
+      fontWeight: 500,
+      fontFamily: "Juana, serif",
+    },
+    {
+      text: "While choosing tiles for a small bathroom can be a bit of a minefield, take heart from the fact that the right designs have the power to make a huge impact – even more so when space is tight. And, as proved by the ideas below, a small shower or bathroom doesn’t need to be plain or lacking in personality: you can create a compact space that still ticks all the boxes when it comes to style and character.",
+      fontSize: "20px",
+      fontWeight: 300,
+      fontFamily: '"Gill Sans", sans-serif',
+    },
+  ],
+  points: [
+    {
+      title: "Switch up your tile sizes",
+      image:
+        "https://www.mandarinstone.com/app/uploads/fly-images/256494/Vita-Ochre-Matt-Porcelain-Kobi-Terra-Gloss-Porcelain-4-scaled-700x0.jpg",
+      text: "It’s true that using the same tiles throughout your space can help to make a small bathroom feel bigger, but this approach can occasionally backfire, particularly if you’re working strong colours or bold patterns. Here, Vita porcelain tiles in Ochre have been paired with our Kobi Terra porcelain design, offering a change in pace thanks to contrasting tile sizes and the combination of patterned with plain. This creates depth and prevents either design from crowding the space, while complementary colours ensure overall effect is visually harmonious.",
+    },
+    {
+      title: "Add interest with delicate mosaics",
+      image:
+        "https://www.mandarinstone.com/app/uploads/fly-images/90043/Calacatta-Amber-Honed-Marble-Hexagon-Mosaic-_-Calacatta-Amber-Honed-Marble-Pluto-Basin-_1_-scaled-700x0.jpg",
+      text: "A small bathroom is the perfect place for intricate marble mosaic tiles, as you can really appreciate their beauty. There’s also less surface area to cover, making them a more affordable option. These hexagon mosaics in honed Calacatta marble have a soft, milk-white colour that’s cut through with subtle amber veining. They’re an excellent choice for behind basins and around vanity units, especially when applied inside an alcove space as shown above. And, all our mosaics are supplied on mesh-back sheets, so while the finished effect is pleasingly intricate, it’s also incredibly easy to achieve.",
+    },
+    {
+      title: "Don’t shy away from texture",
+      image:
+        "https://www.mandarinstone.com/app/uploads/fly-images/256368/Bar-Terra-Ceramic-1-scaled-700x0.jpg",
+      text: "While an understated scheme can often help a poky space feel roomier, we’re not ones to sacrifice character for the sake of adhering to convention. Often, an impactful space is just as enjoyable as one that looks and feels a few inches bigger, which is why we’re great advocates of textured tiles, whatever size your bathroom. One of our current favourites is our Bar ceramic tile in Terra. A wonderfully tactile take on traditional Spanish tiles, their rugged surface is ideal for bringing some texture to a feature wall, vanity area or cloakroom.",
+    },
+    {
+      title: "Build in bathroom storage",
+      image:
+        "https://www.mandarinstone.com/app/uploads/fly-images/232057/Toledo-Sand-Porcelain-Iggy-Sage-Gloss-Porcelain-Mosaic-1-scaled-700x0.jpg",
+      text: "Nothing makes a cramped bathroom feel even smaller than surfaces cluttered with toiletry bottles. That’s why adequate storage is just as important as the design of your tiles, taps and sanitaryware. Where possible, recessed shelving will create extra room without further crowding your space; it can also become a fun feature that really elevates the aesthetic. One such example is this arched shower shelf, which is lined with our Iggy kit-kat porcelain mosaics in Sage. Not only does this break up the wall of expansive Toledo Sand porcelain tiles, it provides the perfect spot to keep your toiletries close to hand.",
+    },
+    {
+      title: "Make clever use of alcove space",
+      image:
+        "https://www.mandarinstone.com/app/uploads/fly-images/239386/VERBENA_MARBLE_IGGY_PASTELLA_VERDE_017-Web-Resolution-700x0.jpg",
+      text: "A small bathroom is the perfect place for intricate marble mosaic tiles, as you can really appreciate their beauty. There’s also less surface area to cover, making them a more affordable option. These hexagon mosaics in honed Calacatta marble have a soft, milk-white colour that’s cut through with subtle amber veining. They’re an excellent choice for behind basins and around vanity units, especially when applied inside an alcove space as shown above. And, all our mosaics are supplied on mesh-back sheets, so while the finished effect is pleasingly intricate, it’s also incredibly easy to achieve.",
+    },
+    {
+      title: "Go for pale tiles and gloss finishes",
+      image:
+        "https://www.mandarinstone.com/app/uploads/fly-images/196878/Paloma-Cream-Gloss-Porcelain-Porcelain-Cream-Matt-Porcelain-scaled-700x0.jpg",
+      text: "While we might not be slaves to small-space decorating clichés, we’re not ones to shun a tried-and-tested route to an effortlessly roomier bathroom. Whether your space is light deprived or flooded with sunshine, pale colours and light-reflecting surfaces are a recipe for success that you can’t go wrong with. Our Paloma porcelain tile in Cream Gloss is a perfect example of a tile that’s pale but interesting, with a warm-beige backdrop and subtle marble-effect veining. Its high-gloss surface is great for bouncing light around a space, while the neutral hue means it pairs well with a host of other designs. Alternatively, use on walls and floors for a serene and spacious scheme.",
+    },
+    {
+      title: "Break up bold colour with surface detailing",
+      image:
+        "https://www.mandarinstone.com/app/uploads/fly-images/256405/Fitz-Green-Honed-Marble-305x75x12mm-2-scaled-700x0.jpg",
+      text: "If you love strong shades but aren’t sure how to make them work in a small bathroom, then rest assured that it is possible. The trick is to choose designs with surface details that offset the effect of the bold colour, whether that’s deliberate imperfections, distressed edges, a subtle pattern or lighter veining. Here, our Fitz honed marble tiles bring an injection of rich green into this bathroom, which is counterbalanced by the design’s natural tonal variation and pale veins. Their slender rectangular format is an added bonus too, as it helps to elongate the wall behind the basin.",
+    },
+    {
+      title: "Stick to large-format tiles",
+      image:
+        "https://www.mandarinstone.com/app/uploads/fly-images/47280/Cemento-White-Matt-Porcelain-Oska-Linen-Porcelain-2-700x0.jpg",
+      text: "Another fail-safe decorating tip for a spatially challenged shower or bathroom is to use large-format tiles on the walls, floors or both. This works because you have less grout lines carving up the space, which can make a room feel smaller than it actually is. The floor of this wet room features our Cemento concrete-effect tiles in a cool-white colourway, which undoubtedly contribute to the sense of spaciousness and airiness. Choosing tiles in a similar tone for the walls (like our Oska matt porcelain range in Linen) also helps to blur the boundary between where the floor stops and the wall begins.",
+    },
+  ],
+};
